01ce2e7f47450338f5d993f8357dec846b89725d
noahmnaigo <150990263+noahmnaigo@users.noreply.github.com>
2024-08-26 23:13:35 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ static/uv/sw.js
@@ -0,0 +1,11 @@
+// UV Transports
+importScripts('/libcurl/index.cjs');
+importScripts('/epoxy/index.js');
+
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts(__uv$config.sw || '/uv/uv.sw.js');
+
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
